9b7e2455c046df3b7fc7237c2a2ae254f8819fae
Mateus Alves <msilva.spa@hotmail.com>
2026-03-24 22:54:16 -0300

    refactor: simplify DDDs definition and update tsup config for sourcemap and minification

---
+++ .changeset/puny-waves-carry.md
@@ -0,0 +1,5 @@
+---
+'brazuka-utils': patch
+---
+
+refactor: simplify DDDs definition and update tsup config for sourcemap and minification
--- src/phone/validate.ts
+++ src/phone/validate.ts
@@ -1,72 +1,15 @@
 // Valid Brazilian DDDs (area codes)
 const VALID_DDDS = new Set([
-  11,
-  12,
-  13,
-  14,
-  15,
-  16,
-  17,
-  18,
-  19, // São Paulo
-  21,
-  22,
-  24, // Rio de Janeiro
-  27,
-  28, // Espírito Santo
-  31,
-  32,
-  33,
-  34,
-  35,
-  37,
-  38, // Minas Gerais
-  41,
-  42,
-  43,
-  44,
-  45,
-  46, // Paraná
-  47,
-  48,
-  49, // Santa Catarina
-  51,
-  53,
-  54,
-  55, // Rio Grande do Sul
-  61, // Distrito Federal
-  62,
-  64, // Goiás
-  63, // Tocantins
-  65,
-  66, // Mato Grosso
-  67, // Mato Grosso do Sul
-  68, // Acre
-  69, // Rondônia
-  71,
-  73,
-  74,
-  75,
-  77, // Bahia
-  79, // Sergipe
-  81,
-  87, // Pernambuco
-  82, // Alagoas
-  83, // Paraíba
-  84, // Rio Grande do Norte
-  85,
-  88, // Ceará
-  86,
-  89, // Piauí
-  91,
-  93,
-  94, // Pará
-  92,
-  97, // Amazonas
-  95, // Roraima
-  96, // Amapá
-  98,
-  99, // Maranhão
+  11, 12, 13, 14, 15, 16, 17, 18, 19, // SP
+  21, 22, 24, 27, 28,                  // RJ, ES
+  31, 32, 33, 34, 35, 37, 38,          // MG
+  41, 42, 43, 44, 45, 46,              // PR
+  47, 48, 49,                          // SC
+  51, 53, 54, 55,                      // RS
+  61, 62, 63, 64, 65, 66, 67, 68, 69,  // DF, GO, TO, MT, MS, AC, RO
+  71, 73, 74, 75, 77, 79,              // BA, SE
+  81, 82, 83, 84, 85, 86, 87, 88, 89,  // PE, AL, PB, RN, CE, PI
+  91, 92, 93, 94, 95, 96, 97, 98, 99,  // PA, AM, RR, AP, MA
 ])
 
 export function validatePhone(value: string): boolean {
